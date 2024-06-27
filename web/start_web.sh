19d4e073eafe8ba153c2315e38363c43ec562d07
leslie2046 <253605712@qq.com>
2024-06-27 09:03:37 +0800

    update shell

--- web/start_web.sh
+++ web/start_web.sh
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 nohup npm run start &> web.log &
 =======
 nohup npm run start &> output.log &
 >>>>>>> 6619e2a3 (add shell to start/kill web)
+=======
+nohup npm run start &> web.log &
+>>>>>>> 0e019f7b (update shell)
