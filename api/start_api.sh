3708dfbf44b6cc70b7b53fd7b4b4a8fd486d0703
leslie2046 <253605712@qq.com>
2024-06-28 10:02:36 +0800

    update start_api.sh

--- api/start_api.sh
+++ api/start_api.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pip3 install -r requirements.txt
+poetry install
 flask db upgrade
 nohup flask run --host 0.0.0.0 --port=6006 --debug &> api.log &
 nohup celery -A app.celery worker -P gevent -c 10 -Q dataset,generation,mail --loglevel DEBUG &> worker.log &
