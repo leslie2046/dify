894d72eb6ff1062ee6f5a90356bda4e8c0033826
leslie2046 <253605712@qq.com>
2024-07-04 14:29:46 +0800

    add shell

--- web/kill_web.sh
+++ web/kill_web.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 kill -9 $(ps -ef|grep 'npm'|grep -v grep|awk '{print $2}')
-<<<<<<< HEAD
-<<<<<<< HEAD
 kill -9 $(ps -ef|grep 'next-server'|grep -v grep|awk '{print $2}')
-=======
->>>>>>> 6619e2a3 (add shell to start/kill web)
-=======
-kill -9 $(ps -ef|grep 'next-server'|grep -v grep|awk '{print $2}')
->>>>>>> 0e019f7b (update shell)
--- web/start_web.sh
+++ web/start_web.sh
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 nohup npm run start &> web.log &
-=======
-nohup npm run start &> output.log &
->>>>>>> 6619e2a3 (add shell to start/kill web)
-=======
-nohup npm run start &> web.log &
->>>>>>> 0e019f7b (update shell)
