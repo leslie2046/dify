d018c07dbb2ba70f550972ccd5f74443a58cbb06
leslie2046 <253605712@qq.com>
2025-01-13 21:12:54 +0800

    modify

--- api/kill_api.sh
+++ api/kill_api.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pkill -f "flask run --host 0.0.0.0 --port=6006 --debug"
-pkill -f 'celery -A app.celery worker -P gevent -c 10 -Q dataset,generation,mail --loglevel DEBUG'
+pkill -f "celery -A app.celery worker -P gevent -c 10 -Q dataset,generation,mail --loglevel DEBUG"
